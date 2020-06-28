1013525ff7a59c470c2126da37b6dd2c54287a31
Vasiliy <63386773+helllrice@users.noreply.github.com>
2020-06-28 10:35:54 +0300

    homework

---
+++ index.js
@@ -0,0 +1,24 @@
+let a = 2;
+let b = 3;
+
+function multiplication(a, b){
+	console.log(a*b);
+};
+
+multiplication(a, b);
+
+
+
+
+
+let y = 10;
+
+function square(a) {
+	console.log(a**2);
+};
+
+square(y);
+
+
+
+
