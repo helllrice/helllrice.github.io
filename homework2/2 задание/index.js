f7bd8cc4961cc1f5eca90712dc5f77ccc1cd6acf
Vasiliy <63386773+helllrice@users.noreply.github.com>
2020-06-28 10:37:06 +0300

    Add files via upload

---
+++ homework2/2 задание/index.js
@@ -0,0 +1,8 @@
+let text = document.querySelector('#text');
+
+
+function change(){
+	text.innerText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est laborum earum quasi quae aliquam, omnis magnam, delectus accusantium iure suscipit placeat libero aliquid, totam non. At facere voluptatem veniam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est laborum earum quasi quae aliquam, omnis magnam, delectus accusantium iure suscipit placeat libero aliquid, totam non. At facere voluptatem veniam.';
+}
+
+
