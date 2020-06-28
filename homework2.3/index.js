363548f634785d8855177f19edd629393ff667cc
Vasiliy <63386773+helllrice@users.noreply.github.com>
2020-06-28 12:12:40 +0300

    Add files via upload

---
+++ homework2.3/index.js
@@ -0,0 +1,14 @@
+const text = document.querySelector('#text');
+
+
+
+function change(){
+	text.innerText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est laborum earum quasi quae aliquam, omnis magnam, delectus accusantium iure suscipit placeat libero aliquid, totam non. At facere voluptatem veniam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est laborum earum quasi quae aliquam, omnis magnam, delectus accusantium iure suscipit placeat libero aliquid, totam non. At facere voluptatem veniam.';
+	element.style.display = "none";
+	container.style.height = "310px";
+
+}
+
+
+
+
