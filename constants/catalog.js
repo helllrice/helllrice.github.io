8ecf17fa7ea2b6fe852bbbb2f1054de6203d26e2
Vasiliy <63386773+helllrice@users.noreply.github.com>
2020-08-15 22:16:49 +0300

    Add files via upload

---
+++ constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Iphone SE',
+        img: 'https://i.ibb.co/dGrTqgF/iphone-compare-se-202003.png',
+        price: 39990,
+    },
+    {
+        id: 'el2',
+        name: 'Iphone 11',
+        img: 'https://i.ibb.co/vXrhYKH/iphone-compare-11-202003.png',
+        price: 59990,
+    },
+    {
+        id: 'el3',
+        name: 'iPhone 11 Pro',
+        img: 'https://i.ibb.co/gV5hR4J/iphone-compare-11pro-202003.png',
+        price: 89990,
+    },
+    {
+        id: 'el4',
+        name: 'iPhone XR',
+        img: 'https://i.ibb.co/M8Swwff/compare-iphone-xr-dexbg7jerlqq-large-2x.jpg',
+        price: 49990,
+    },
+    {
+        id: 'el5',
+        name: 'iPhone 7',
+        img: 'https://avatars.mds.yandex.net/get-mpic/466729/img_id5074426843449101979.jpeg/9hq',
+        price: 24990,
+    },
+    {
+        id: 'el6',
+        name: 'iPhone 6s',
+        img: 'https://avatars.mds.yandex.net/get-mpic/1525355/img_id8139261268256005043.jpeg/9hq',
+        price: 18990,
+    },
+    {
+        id: 'el7',
+        name: 'iPhone 8',
+        img: 'https://avatars.mds.yandex.net/get-mpic/932277/img_id8577152390218330050.jpeg/9hq',
+        price: 33990,
+    },
+    {
+        id: 'el8',
+        name: 'iPhone 8+',
+        img: 'https://avatars.mds.yandex.net/get-mpic/331398/img_id316797945473503285.jpeg/6hq',
+        price: 46990,
+    },
+    {
+        id: 'el9',
+        name: 'iPhone 5s',
+        img: 'https://avatars.mds.yandex.net/get-mpic/1374520/img_id1202805144443115156.jpeg/9hq',
+        price: 10990,
+    },
+    {
+        id: 'el10',
+        name: 'iPhone X',
+        img: 'https://avatars.mds.yandex.net/get-mpic/200316/img_id270362589725797013.png/6hq',
+        price: 50990,
+    },
+];
